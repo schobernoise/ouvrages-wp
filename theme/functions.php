052f84c5a007624e8e107febe0da5ac06dba1ff8
Fabian Schober <reach@fabianschober.com>
2024-03-22 16:06:29 +0100

    added custom logo support

--- theme/functions.php
+++ theme/functions.php
@@ -104,6 +104,8 @@ if (!function_exists('ouvrages_wp_setup')) :
 			)
 		);
 
+		add_theme_support('custom-logo');
+
 		// Add theme support for selective refresh for widgets.
 		add_theme_support('customize-selective-refresh-widgets');
 
